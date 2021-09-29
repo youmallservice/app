06fe3b5487e623e49587674ffb9e6b3f10ba9a34
iAriel <ariel@aluno.fapce.edu.br>
2021-09-29 15:13:52 -0300

    [FRONT] animation and placement of adjusted cards

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -10,7 +10,26 @@ export default function HomePage() {
             <Nav />
             <div id="page-content">
               {/* <Caroussel /> */}
-              <Card />
+                <div className="product-rows section">
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+                        <div className="section-header text-center">
+                          <h2 className="h2">Populares</h2>
+                          <p>Produtos mais vistos pelos usuários</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="grid-products">
+                      <div className="row">
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                      </div>
+                    </div>
+                  </div>
+                </div>
             </div>
         </>
     )
